6ab06a6345578131da35e0240a5c417f0e2d6e7d
shadowfax <unaputacuenta@hotmail.com>
2013-04-14 14:35:43 +0200

    SEO data removed on error pages and robots set to nofollow and noindex.

--- README.md
+++ README.md
@@ -5,9 +5,25 @@ Setup
 -----
 In order to autoload the library and make everything work we should edit
 application.ini
-	; --- ZendX ---
-	autoloaderNamespaces.ZendX = "ZendX_"
-	resources.view.helperPath.ZendX_View_Helper = APPLICATION_PATH "/../library/ZendX/View/Helper"
+    ; --- ZendX ---
+    autoloaderNamespaces.ZendX = "ZendX_"
+    resources.view.helperPath.ZendX_View_Helper = APPLICATION_PATH "/../library/ZendX/View/Helper"
+
+ZendX_Seo
+---------
+It can be loaded from application.ini and allows for some SEO (Search Engine Optimization) features.
+
+We can define the prefered domain if we are using multiple domain aliases 
+for things as canonical links. Define some default keywords that should
+appear on every page...
+
+    ; --- ZendX_Seo Setup ---
+    resources.seo.domain = "www.yourdomain.com"
+    resources.seo.keywords = "this, that, other"
+	resources.seo.robots = "follow,index"
+	
+resources.seo.robots defines the default robots policy.
+   
 
 ZendX_Validate_Username
 -----------------------
--- Seo/Controller/Plugin/Seo.php
+++ Seo/Controller/Plugin/Seo.php
@@ -42,6 +42,12 @@ class ZendX_Seo_Controller_Plugin_Seo extends Zend_Controller_Plugin_Abstract
 	
 	// ToDo: Add constructor and allow options
 	//       SEO object MUST be initialized here
+	public function __construct()
+	{
+		$this->getSeo();
+	}
+	
+	private $_processed = false;
 	
 	/**
 	 * Retrieve SEO object.
@@ -60,53 +66,111 @@ class ZendX_Seo_Controller_Plugin_Seo extends Zend_Controller_Plugin_Abstract
 		return $this->_seo;
 	}
 	
+	
+	protected function getMetaTags($html)
+	{
+		preg_match_all('/<meta[^>]+name=\\"([^\\"]*)\\"[^>]+content=\\"([^\\"]*)\\"[^>]+>/i',  $html, $out,PREG_PATTERN_ORDER);
+
+    	$meta['raw'] = $out[0];
+    	$meta['name'] = $out[1];
+    	$meta['content'] = $out[2];
+    	
+    	return $meta;
+	}
+	
 	/**
      * preDispatch() plugin hook -- set the SEO tags
      *
      * @param  Zend_Controller_Request_Abstract $request
      * @return void
      */
-	public function preDispatch(Zend_Controller_Request_Abstract $request)
+	public function postDispatch(Zend_Controller_Request_Abstract $request)
 	{
-		$this->getSeo();
+		if (!$this->_processed) {
+			$response = $this->getResponse();
+			
+			$headers = $response->getHeaders();
+			foreach($headers as $header)
+			{
+				//Do not proceed if content-type is not html/xhtml or such
+				if($header['name'] == 'Content-Type' && strpos($header['value'], 'html') === false)
+					return;			
+			}
+			
+			// Get the SEO resource from registry
+			if ((null !== $this->_seo) && ($request->isDispatched())) {
+				$response = $this->getResponse();
+				$view = Zend_Controller_Action_HelperBroker::getExistingHelper('ViewRenderer')->view;
+				
+				// --- META ---
+				$keywords = $this->_seo->getKeywords();
+				if (is_array($keywords)) {
+					if (count($keywords) > 0) {
+						$keywords = implode(',', $keywords);
+						$view->headMeta()->appendName('keywords', $keywords);
+					}
+				}
+				unset($keywords);
+				
+				$robots = $this->_seo->getRobots();
+				if (is_array($robots)) {
+					if (count($robots) > 0) {
+						$robots = implode(',', $robots);
+						$view->headMeta()->appendName('robots', $robots);
+					}
+				}
+				unset($robots);
+			}
+			
+			$this->_processed = true;
+		}
+	}
+	
+	public function dispatchLoopShutdown()
+	{
+		/**
+		 * Response object
+		 * @var Zend_Controller_Response_Abstract
+		 */
+		$response = $this->getResponse();
 		
-		// Get the SEO resource from registry
-		if (null !== $this->_seo) {
-			$view = Zend_Controller_Action_HelperBroker::getExistingHelper('ViewRenderer')->view;
-			
-			// --- META ---
-			$keywords = $this->_seo->getKeywords();
-			if (is_array($keywords)) {
-				if (count($keywords) > 0) {
-					$keywords = implode(',', $keywords);
-					$view->headMeta()->appendName('keywords', $keywords);
-				}
-			}
-			unset($keywords);
-			
-			$robots = $this->_seo->getRobots();
-			if (is_array($robots)) {
-				if (count($robots) > 0) {
-					$robots = implode(',', $robots);
-					$view->headMeta()->appendName('robots', $robots);
-				}
-			}
-			unset($robots);
-			
-			// --- LINKS ---
-			
-			// Canonical
-			/*
-			$view->headLink()->headLink(
-				array(
-					'rel' => 'canonical', 
-					'href' => 'http://localhost/'
-				)
-			);
-			*/
+		$headers = $response->getHeaders();
+		foreach($headers as $header)
+		{
+			//Do not proceed if content-type is not html/xhtml or such
+			if($header['name'] == 'Content-Type' && strpos($header['value'], 'html') === false)
+				return;			
 		}
 		
+		// Load the page
+		$html = $response->getBody();
 		
+		// METAS
+		$metas = $this->getMetaTags($html);
+		
+		$httpCode = trim($response->getHttpResponseCode());
+		if (($httpCode[0] === "4") || ($httpCode[0] === "5")) {
+			for ($i=0;$i<count($metas['raw']);$i++) {
+				if (strcasecmp($metas['name'][$i], 'robots') === 0) {
+					$html = str_replace($metas['raw'][$i], '<meta name="robots" content="noindex, nofollow" >', $html);
+				} elseif (((strcasecmp($metas['name'][$i], 'keywords') === 0) || (strcasecmp($metas['name'][$i], 'description')) === 0)) {
+					// Remove keywords and description
+					$html = str_replace($metas['raw'][$i], '', $html);
+				}
+			}
+			
+			// Remove Dublin Core (DC) Meta tags
+			$html = preg_replace('/<meta[^>]+name=\\"dc\.([^\\"]*)\\"[^>]+content=\\"([^\\"]*)\\"[^>]+>/i', '', $html);
+			// Remove Open Graph (OG) Meta tags
+			$html = preg_replace('/<meta[^>]+property=\\"og\:([^\\"]*)\\"[^>]+content=\\"([^\\"]*)\\"[^>]+>/i', '', $html);
+			
+			// Finally make the code look better
+			$html = preg_replace('/([\r\n]+)/',"\n", $html);
+			$html = preg_replace('/\n+/',"\n", $html);
+		}
+
+		$response->setBody($html);
 	}
+	
 	
 }
